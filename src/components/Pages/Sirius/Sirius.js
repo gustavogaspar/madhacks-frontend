b58fb44c836e6daaae0b740c299281ae1e0c9a0b
gustavogaspar <gaspar.gualves@gmail.com>
2019-11-04 21:36:22 -0300

    no nlp

--- src/components/Pages/Sirius/Sirius.js
+++ src/components/Pages/Sirius/Sirius.js
@@ -67,7 +67,7 @@ class Sirius extends Component {
             'Cookie': this.state.cookie
           }
         let  params = {resposta: btValue}
-        let res = await axios.post('http://132.145.163.158:5000/tag', params, {headers: dados})
+        let res = await axios.post('http://132.145.163.158:5000/tag', params, {headers: dados},{withCredentials:true})
         console.log( await res.data)
         if (res.data.offset === 0 ){
         let tempArray = res.data.Respostas.map(igKey => igKey)
