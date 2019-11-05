d18bcf16e4302a0916ac0a82e2089b19784200e0
gustavogaspar <gaspar.gualves@gmail.com>
2019-11-05 20:00:39 -0300

    final build

--- src/components/Pages/Sirius/Sirius.js
+++ src/components/Pages/Sirius/Sirius.js
@@ -1,9 +1,9 @@
-import { Form, Button, Header, ButtonGroup } from "semantic-ui-react";
+import { Form, Button, Header, ButtonGroup, Grid } from "semantic-ui-react";
 import React, { Component } from 'react';
 import classes from './Sirius.module.css';
 import axios from 'axios';
 import Recomendacao from './Recomendacao/Recomendacao';
-//import socketIOClient from "socket.io-client";
+import socketIOClient from "socket.io-client";
 
 
 class Sirius extends Component {
@@ -12,7 +12,7 @@ class Sirius extends Component {
         super(props)
 
         this.state = {
-            question: "What do you want to be?",
+            question: "What do you want?",
             value: " ",
             cookie: null,
             answers: [" "],
@@ -25,6 +25,10 @@ class Sirius extends Component {
     }
 
     componentDidMount() {
+        const socket = socketIOClient('https://4000-cadc0d02-8bcf-4120-9253-5a79a257a6be.ws-us02.gitpod.io/')
+        socket.on('result', (payload) => {
+            console.log('[PAYLOAD] -', payload)
+        })
     }
 
 
@@ -41,16 +45,23 @@ class Sirius extends Component {
 
     async handleSubmit(event) {
         console.log('nlpRequest: ' + this.state.value);
-        //IMPLEMENTAR BUSCA NO BOT COM SOCKET.IO
-        //const socket = socketIOClient('https://4000-cadc0d02-8bcf-4120-9253-5a79a257a6be.ws-us1.gitpod.io/')
-        //socket.emit('teste', (this.state.value))
-        const tags = this.state.value
-        console.log(this.state.tags)
-        const url = 'http://132.145.163.158:5000/tag?tag=' + tags;
-        let res = await axios.get(url, {withCredentials:true});
-        let data =  await res.data;
+        const nlpReq = this.state.value
+        let dados = {
+            'Content-Type': 'application/json'
+        }
+        let params = {
+            user: "frontEnd",
+            text: nlpReq
+        }
+        this.setState({question: 'Just a second, Sirius is thinking'})
+        let botRes = await axios.post('https://948f14be.ngrok.io/test/message', params, { headers: dados });
+        console.log(botRes)
+
+        const urlToBackend = 'http://132.145.163.158:5000/tag?tag=' + botRes.data.messagePayload.text;
+
+        let res = await axios.get(urlToBackend, { withCredentials: true });
+        let data = await res.data;
         let cookie = await res.data.sessionid;
-        console.log(res.headers)
         console.log(res.data)
         let tempArray = res.data.Resposta.map(igKey => igKey)
         console.log(tempArray)
@@ -59,46 +70,54 @@ class Sirius extends Component {
     }
 
     async handleQuestion(btValue) {
-        
+
         console.log('Botão Escolhido: ' + btValue);
-        console.log('[SESSION] -',this.state.cookie)
+        console.log('[SESSION] -', this.state.cookie)
         let dados = {
             'Content-Type': 'application/json',
-            'Cookie': this.state.cookie
-          }
-        let  params = {resposta: btValue}
-        let res = await axios.post('http://132.145.163.158:5000/tag', params, {headers: dados},{withCredentials:true})
-        console.log( await res.data)
-        if (res.data.offset === 0 ){
-        let tempArray = res.data.Respostas.map(igKey => igKey)
-        console.log(tempArray)
-        this.setState({ question: res.data.Pergunta, answers: tempArray })
-        console.log(this.state.question, this.state.answers)
+            'Set-Cookie': this.state.cookie
+        }
+        let params = { resposta: btValue }
+        let res = await axios.post('http://132.145.163.158:5000/tag', params, { headers: dados }, { withCredentials: true })
+        console.log(await res.data)
+        if (res.data.offset === 0) {
+            let tempArray = res.data.Respostas.map(igKey => igKey)
+            console.log(tempArray)
+            this.setState({ question: res.data.Pergunta, answers: tempArray })
+            console.log(this.state.question, this.state.answers)
         }
         else {
             let courseArray = res.data.Recomendacao.map(igKey => igKey)
-            this.setState({ courses: courseArray, offset: res.data.offset})
-            console.log(this.state.courses,this.state.offset)
+            this.setState({ courses: courseArray, offset: res.data.offset })
+            console.log(this.state.courses, this.state.offset)
         }
-       }
+    }
 
     render() {
         return (
             <div className={classes.Sirius}>
                 {this.state.cookie ?
                     <> {!this.state.offset ?
-                    <>
-                    <Header as='h1' inverted className={classes.Header}>{this.state.question}</Header>
-                    <ButtonGroup className={classes.ButtonGroup}>
-                        {this.state.answers.map(igKey => (
-                            <Button
-                                onClick={() => this.handleQuestion(igKey)}>
-                                {igKey}</Button>
-                        ))}
-                    </ButtonGroup></>: 
-                    <Recomendacao 
-                    courses={this.state.courses}
-                    />}
+                        <>
+                            <Grid className={classes.Grid} >
+                                <Grid.Row> 
+                                    <h1 className={classes.Header}>{this.state.question}</h1>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <ButtonGroup className={classes.ButtonGroup}>
+
+                                        {this.state.answers.map(igKey => (
+                                            <Button
+                                                onClick={() => this.handleQuestion(igKey)}
+                                                color='orange'>
+                                                {igKey}</Button>
+                                        ))}
+                                    </ButtonGroup>
+                                </Grid.Row>
+                            </Grid></> :
+                        <Recomendacao
+                            courses={this.state.courses}
+                        />}
                     </>
                     :
                     <Form onSubmit={this.handleSubmit}>
